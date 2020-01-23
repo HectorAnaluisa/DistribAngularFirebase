97e0ace97c2cde4ba3ee55d85336cf88a0cbd9ae
HectorAnaluisa <hector.analuisa@epn.edu.ec>
2020-01-23 17:26:10 -0500

    Añado

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ConsumorestService } from './service/consumorest.service'; 
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demo';
+
+  categoria:  any = []; 
+  constructor(public rest: ConsumorestService){}
+
+  ngOnInit(){
+    this.getCategorias(); 
+  }
+
+
+  getCategorias(){
+
+    this.categoria = []; 
+    this.rest.getCategoriasRest().subscribe(data => {
+      console.log(data); 
+      this.categoria = data; 
+    }); 
+
+
+  }
+
+
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyp72DGV14KDKXjUumq2p7LmBOKLJqeaY",
+    authDomain: "holabb-9eafa.firebaseapp.com",
+    databaseURL: "https://holabb-9eafa.firebaseio.com",
+    projectId: "holabb-9eafa",
+    storageBucket: "holabb-9eafa.appspot.com",
+    messagingSenderId: "793687837728",
+    appId: "1:793687837728:web:e54f49fca98960f3cd4045",
+    measurementId: "G-1XQS7QS3H9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
